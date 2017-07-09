bd4c61fe8beee377c9f1b798151a097a7c4f3bc1
Juan Antonio Orozco bianchi <iam@mexcoder.com>
2017-07-09 09:30:09 -0500

    updated CV content, script cleanup
    Signed-off-by: Juan Antonio Orozco bianchi <iam@mexcoder.com>

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ set lang=en es
 set theme=banking casual classic oldStyle
 
 if not exist "auxFiles/" call makeaux
-if not exist "out/" call makeout rem lol! this was not intentional
+if not exist "out/" call makeout
 
 FOR %%c in (%lang%) do (
     if not exist "out/%%c" call :makelang %%c
@@ -18,11 +18,6 @@ FOR %%b in (%lang%) DO (
     )
 )
 
-rem old way
-rem pdflatex cv_banking.tex -aux-directory=auxFiles -output-directory=out/es
-rem pdflatex cv_casual.tex -aux-directory=auxFiles -output-directory=out/es
-rem pdflatex cv_classic.tex -aux-directory=auxFiles -output-directory=out/es
-rem pdflatex cv_oldStyle.tex -aux-directory=auxFiles -output-directory=out/es
 
 goto fin
 
